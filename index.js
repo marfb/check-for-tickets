36bdfbe6812fdd9221f9bc375090d89a4bdbef63
Martín Bello <marferbe@gmail.com>
2023-02-28 19:37:53 -0300

    added send email if something happens and control to clearInterval

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
 const cheerio = require("cheerio");
 const axios = require("axios");
 const constants = require("./CONFIG");
-const { ACCOUNT_SID, AUTH_TOKEN, FROM_NUMBER, TO_NUMBER } = constants;
+const {
+  ACCOUNT_SID,
+  AUTH_TOKEN,
+  FROM_NUMBER,
+  TO_NUMBER,
+  MAIL,
+  SMTP_PORT,
+  SMTP_HOST,
+  MAIL_USER,
+  MAIL_PASS,
+  MAIL_FROM,
+} = constants;
 const client = require("twilio")(ACCOUNT_SID, AUTH_TOKEN);
+const nodemailer = require("nodemailer");
+
+let mailSent = false;
+let smsSent = false;
+let timesExecuted = 1;
 
 const performScraping = async () => {
   const axiosResponse = await axios.request({
@@ -17,6 +33,39 @@ const performScraping = async () => {
   const $ = cheerio.load(axiosResponse.data);
   const eventCards = $(".container.marketing div.evento a h2");
 
+  timesExecuted += 1;
+  if (mailSent || smsSent) {
+    console.log("process done");
+    clearInterval(process);
+  }
+
+  if (eventCards.length < 1 && !mailSent) {
+    const transporter = nodemailer.createTransport({
+      sendmail: true,
+      port: SMTP_PORT,
+      host: SMTP_HOST,
+      secure: true,
+      auth: {
+        user: MAIL_USER,
+        pass: MAIL_PASS,
+      },
+    });
+    transporter.sendMail(
+      {
+        from: MAIL_FROM,
+        to: `${MAIL}`,
+        subject: "No hay eventos en AUTOENTRADA",
+        text: "Este mensaje es para avisarte que no hay eventos en autoentrada. Puede deberse a que no hay más eventos disponibles o a que el selector para encontrar los eventos haya cambiado.",
+      },
+      (err, info) => {
+        if (err) {
+          console.log(err);
+        }
+        mailSent = true;
+      }
+    );
+  }
+
   let notify = false;
 
   eventCards.each((_, event) => {
@@ -27,15 +76,21 @@ const performScraping = async () => {
     }
   });
 
-  if (notify) {
+  if (notify && !smsSent) {
     client.messages
       .create({
         body: "Entradas a la venta!",
         from: `${FROM_NUMBER}`,
         to: `${TO_NUMBER}`,
       })
-      .then((message) => console.log(message.sid));
+      .then((message) => {
+        smsSent = true;
+        console.log(message.sid);
+      });
   }
 };
 
-performScraping();
+const process = setInterval(
+  () => performScraping().then(console.log(`executed ${timesExecuted} times`)),
+  10000
+);
--- template.CONFIG.js
+++ template.CONFIG.js
@@ -3,4 +3,10 @@ module.exports = {
   AUTH_TOKEN: "your-twilio-auth-token",
   FROM_NUMBER: "your-twilio-phone-number",
   TO_NUMBER: "your-own-cellphone-number",
+  MAIL: "your-mail@address.com",
+  SMTP_PORT: 465,
+  SMTP_HOST: "your-smtp-host",
+  MAIL_USER: "user-smtp-host",
+  MAIL_PASS: "pass-smtp-host",
+  MAIL_FROM: "sender@mail.com",
 };
